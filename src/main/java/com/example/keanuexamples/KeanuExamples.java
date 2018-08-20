eb9b327ba7aca576727aad55395ac44736f18989
Joanne Hong <joannehong@improbable.io>
2018-08-20 15:00:31 +0100

    use prior sampling method

---
+++ src/main/java/com/example/keanuexamples/KeanuExamples.java
@@ -0,0 +1,7 @@
+package com.example.keanuexamples;
+
+public class KeanuExamples {
+    public static void main(String[] args) {
+        System.out.println(new ApproximatePi().run(100000, 10000));
+    }
+}
